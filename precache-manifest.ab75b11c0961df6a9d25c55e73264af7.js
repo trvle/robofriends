78ff281b86c3b9b99578cf7b9ed4e66327627a3b
trvle <travle58@hotmail.com>
2019-02-06 21:46:41 -0500

    Updates

---
+++ precache-manifest.ab75b11c0961df6a9d25c55e73264af7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "99670d48b95672a1c3bf",
+    "url": "/robofriends/static/js/main.99670d48.chunk.js"
+  },
+  {
+    "revision": "574f0b4d6a0dcb510c84",
+    "url": "/robofriends/static/js/1.574f0b4d.chunk.js"
+  },
+  {
+    "revision": "99670d48b95672a1c3bf",
+    "url": "/robofriends/static/css/main.3608905d.chunk.css"
+  },
+  {
+    "revision": "574f0b4d6a0dcb510c84",
+    "url": "/robofriends/static/css/1.1e677976.chunk.css"
+  },
+  {
+    "revision": "4104fa3757c11e5b43cde1c58a58e500",
+    "url": "/robofriends/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "trvle.github.io/robofriends/precache-manifest.56f7363078988a4649c5ca47797c2a3f.js"
+  "/robofriends/precache-manifest.ab75b11c0961df6a9d25c55e73264af7.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("trvle.github.io/robofriends/index.html", {
+workbox.routing.registerNavigationRoute("/robofriends/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
