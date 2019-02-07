39fc601128056eac68257fc04b710a554e1ceedc
trvle <travle58@hotmail.com>
2019-02-06 21:29:57 -0500

    Updates

---
+++ precache-manifest.56f7363078988a4649c5ca47797c2a3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "trvle.github.io/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "959c54a784fffbb79217",
+    "url": "trvle.github.io/robofriends/static/js/runtime~main.959c54a7.js"
+  },
+  {
+    "revision": "cbfc892abf37ebbd0b4b",
+    "url": "trvle.github.io/robofriends/static/js/main.cbfc892a.chunk.js"
+  },
+  {
+    "revision": "574f0b4d6a0dcb510c84",
+    "url": "trvle.github.io/robofriends/static/js/1.574f0b4d.chunk.js"
+  },
+  {
+    "revision": "cbfc892abf37ebbd0b4b",
+    "url": "trvle.github.io/robofriends/static/css/main.5a0631ae.chunk.css"
+  },
+  {
+    "revision": "574f0b4d6a0dcb510c84",
+    "url": "trvle.github.io/robofriends/static/css/1.1e677976.chunk.css"
+  },
+  {
+    "revision": "f213fc65650559145b8561f5585aa808",
+    "url": "trvle.github.io/robofriends/index.html"
+  }
+];
